52fc683dd8dfa65e65359dfe5af062f964180daa
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-05-28 17:56:22 -0500

    the entire structure and style of the screen hero component was created.

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,18 +1,54 @@
-import { useParams,Navigate } from "react-router-dom";
+import { useParams, Navigate, useNavigate } from "react-router-dom";
 import { getHeroeById } from "../../selectors/getHeroById";
 
 export const HeroScreen = () => {
-  const { id } = useParams();
+  const { HeroeId } = useParams();
+  const navigate = useNavigate();
 
-  const hero = getHeroeById(id);
+  const hero = getHeroeById(HeroeId);
 
-if(!hero){
-  return <Navigate to='/' />
-}
+  if (!hero) {
+    return <Navigate to="/" />;
+  }
+
+  const handleReturn =()=>{
+    // publisher==='DC Comics' ? navigate('/dc'): navigate('/marvel')
+    navigate(-1)
+
+  }
+
+  const { id, superhero, publisher, alter_ego, first_appearance, characters } =
+    hero;
+  const imagePath = `/assets/${id}.jpg`;
   return (
-    <div>
-      <h1>HeroScreen</h1>
-      <p>{hero.superhero}</p>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img src={imagePath} alt={superhero} className="img-thumbnail" />
+      </div>
+
+      <div className="col-8">
+        <h3> {superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <b>Alter ego:</b> {alter_ego}
+          </li>
+          <li className="list-group-item">
+            <b>Publisher:</b> {publisher}
+          </li>
+          <li className="list-group-item">
+            <b>first_appearance</b> {first_appearance}
+          </li>
+        </ul>
+
+        <h5 className="mt-3">Characters</h5>
+        <p>{ characters }</p>
+
+        <button className="btn btn-outline-info"
+        onClick={handleReturn}
+        >
+          Regresar
+        </button>
+      </div>
     </div>
   );
 };
