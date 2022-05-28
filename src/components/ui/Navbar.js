02b407d54ec2a1dce4fa0b55e99a7826638e326f
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-05-28 00:26:14 -0500

    the logout button and the handlelogout function have been created and the user name has been created

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -2,6 +2,11 @@ import { Link, NavLink } from 'react-router-dom';
 
 
 export const Navbar = () => {
+
+    const handleLogout=()=>{
+        //TODO: Por Hacer
+        console.log('logout')
+    }
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
             
@@ -16,14 +21,14 @@ export const Navbar = () => {
                 <div className="navbar-nav">
 
                     <NavLink 
-                        className="nav-item nav-link" 
+                        className={({isActive})=>"nav-item nav-link " + (isActive && 'active') }
                         to="/marvel"
                     >
                         Marvel
                     </NavLink>
 
                     <NavLink 
-                        className="nav-item nav-link" 
+                        className={({isActive})=>"nav-item nav-link " + (isActive && 'active') }
                         to="/dc"
                     >
                         DC
@@ -31,14 +36,18 @@ export const Navbar = () => {
                 </div>
             </div>
 
-            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
+            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
-                    <NavLink 
-                        className="nav-item nav-link" 
-                        to="/login"
+
+                    <span className='nav-item nav-link text-info'>
+                        Camilo
+                    </span>
+                    <button 
+                        className="nav-item nav-link btn"
+                        onClick={handleLogout}
                     >
                         Logout
-                    </NavLink>
+                    </button>
                 </ul>
             </div>
         </nav>
