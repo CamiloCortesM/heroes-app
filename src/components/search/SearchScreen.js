4762d2837d9935fe654b61e3c40ec26efe058b92
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-05-29 14:53:42 -0500

    implemented search design where custom hooks are used to store the input value

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,9 +1,47 @@
 import React from "react";
+import { useForm } from "../../hooks/useForm";
 
 export const SearchScreen = () => {
+
+  const [values,handleInputChange]= useForm({
+    searchText:'',
+  })
+
+  const {searchText} = values;
+
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+    console.log(searchText)
+  }
   return (
-    <div>
-      <h1>SearchScreen</h1>
-    </div>
+    <>
+      <h1>Searchs</h1>
+      <hr />
+
+      <div className="row">
+        <div className="col-5">
+            <h4>Search</h4>
+            <hr />
+
+            <form  onSubmit={handleSubmit}>
+            <input type="text"
+            placeholder="seach a hero"
+            className="form-control"
+            name="searchText"
+            autoComplete="off"
+            value={searchText}
+            onChange={handleInputChange}/>
+
+            <button
+            className="btn btn-outline-primary mt-1"
+            type="submit"
+            >
+              Buscar...
+            </button>
+
+            </form>
+        </div>
+      </div>
+    </>
   );
 };
