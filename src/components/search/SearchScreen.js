62192e7d30941d4b588290941e42dd6ac530d15b
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-05-29 19:55:51 -0500

    useNavigate and useLocation were used for the parameters and search for a hero in this component

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,18 +1,28 @@
 import React from "react";
+import queryString from 'query-string';
+import { useNavigate, useLocation } from "react-router-dom";
 import { useForm } from "../../hooks/useForm";
+import { getHeroeByName } from "../../selectors/getHeoresByName";
+import HeroCard from "../hero/HeroCard";
 
 export const SearchScreen = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
 
-  const [values,handleInputChange]= useForm({
-    searchText:'',
-  })
+  const {q = ''} = queryString.parse(location.search);
 
-  const {searchText} = values;
+  const [values, handleInputChange] = useForm({
+    searchText: q,
+  });
 
-  const handleSubmit =(e)=>{
+  const { searchText } = values;
+
+  const heroesFileted = getHeroeByName(q);
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(searchText)
-  }
+    navigate(`?q=${searchText}`)
+  };
   return (
     <>
       <h1>Searchs</h1>
@@ -20,26 +30,33 @@ export const SearchScreen = () => {
 
       <div className="row">
         <div className="col-5">
-            <h4>Search</h4>
-            <hr />
+          <h4>Search</h4>
+          <hr />
 
-            <form  onSubmit={handleSubmit}>
-            <input type="text"
-            placeholder="seach a hero"
-            className="form-control"
-            name="searchText"
-            autoComplete="off"
-            value={searchText}
-            onChange={handleInputChange}/>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="seach a hero"
+              className="form-control"
+              name="searchText"
+              autoComplete="off"
+              value={searchText}
+              onChange={handleInputChange}
+            />
 
-            <button
-            className="btn btn-outline-primary mt-1"
-            type="submit"
-            >
+            <button className="btn btn-outline-primary mt-1" type="submit">
               Buscar...
             </button>
+          </form>
+        </div>
 
-            </form>
+        <div className="col-7">
+          <h4>Result</h4>
+          <hr />
+
+          {heroesFileted.map((hero) => (
+            <HeroCard key={hero.id} {...hero} />
+          ))}
         </div>
       </div>
     </>
