47ff5b3b477a1d48763b8ace021c258478b81a4e
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-05-29 19:54:41 -0500

    a function has been created that returns an array of heroes

---
+++ src/selectors/getHeoresByName.js
@@ -0,0 +1,9 @@
+import { heroes } from "../data/heroes";
+
+
+export const getHeroeByName = (name ='')=>{
+    if(name===''){
+        return [];
+    }
+    return heroes.filter(hero => hero.superhero.toLowerCase().includes(name.toLowerCase())) || []
+}
