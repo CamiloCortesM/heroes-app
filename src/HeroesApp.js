ddc29adf251199c68ae95021d4f607ee4e53c08e
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-05-30 23:39:02 -0500

    user in localStorage was created and

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
 import { AuthContext } from "./auth/authContext";
 import { authReducer } from "./auth/authReducer";
 import { AppRouter } from "./routes/AppRouter";
@@ -8,6 +8,12 @@ const init = () => {
 };
 const HeroesApp = () => {
   const [user, dispatch] = useReducer(authReducer, {}, init);
+
+  useEffect(() => {
+    if (!user) return;
+
+    localStorage.setItem("user", JSON.stringify(user));
+  }, [user]);
 
   return (
     <AuthContext.Provider
