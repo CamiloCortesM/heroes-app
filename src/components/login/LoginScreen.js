8ea91b40231c51707d142312d2d5efefcb56f8e3
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-05-30 23:41:23 -0500

    the useReducer dispatch was used for the login user

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,8 +1,19 @@
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../auth/authContext";
+import { types } from "../../types/types";
 
 export const LoginScreen = () => {
+  const { dispatch } = useContext(AuthContext);
   const navigate = useNavigate();
   const handleLogin = () => {
+
+    dispatch({
+      type:types.login,
+      payload:{
+        name:'Camilo',
+      }
+    })
     navigate("/marvel", {
         replace: true
     });
