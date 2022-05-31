2cbec51ea0a78d5770e86de129ab8b2b6e01b24e
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-05-30 23:40:08 -0500

    user in localStorage was created and extracted for use in useReducer

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -9,9 +9,9 @@ const init = () => {
 const HeroesApp = () => {
   const [user, dispatch] = useReducer(authReducer, {}, init);
 
+  
   useEffect(() => {
     if (!user) return;
-
     localStorage.setItem("user", JSON.stringify(user));
   }, [user]);
 
